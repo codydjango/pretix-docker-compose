0debb0c7b7028ee18b154bf584e0b74ffc8b85d4
Cody Django Redmond <codydjango@gmail.com>
2019-01-11 16:49:06 -0800

    add notes

--- bin/commands.sh
+++ bin/commands.sh
@@ -7,7 +7,7 @@ alias dcl="docker-compose logs -f"
 
 function generateCert {
 	# https://letsencrypt.org/docs/certificates-for-localhost/
-	# https://github.com/jwilder/nginx-proxy
+	# https://medium.freecodecamp.org/how-to-get-https-working-on-your-local-development-environment-in-5-minutes-7af615770eec
 	echo "Creating cert for ${1}";
 
 	DOMAIN=$1;
