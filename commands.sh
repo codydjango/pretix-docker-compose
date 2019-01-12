0ab22f3fb5faa421aafd5a65a046b6d037d6a29a
Cody Django Redmond <codydjango@gmail.com>
2019-01-11 16:20:24 -0800

    add gitignore, update commands, add in ssl certificate support

--- commands.sh
+++ commands.sh
@@ -3,5 +3,26 @@ alias dr="docker run --rm -p 8000:8000 -it sss"
 alias dcb="docker-compose build"
 alias dcu="docker-compose up"
 alias dcd="docker-compose down"
+alias dcl="docker-compose logs -f"
+
+
+function generateCert {
+	# https://letsencrypt.org/docs/certificates-for-localhost/
+	# https://github.com/jwilder/nginx-proxy
+	echo "Creating cert for ${1}";
+
+	DOMAIN=$1;
+	KEY="${DOMAIN}.key";
+	CRT="${DOMAIN}.crt";
+	ENCRYPT="rsa:2048"; #"rsa:2048";"4096"
+	SUBJ="/CN=${DOMAIN}";
+	CONFIG="[dn]\nCN=${DOMAIN}\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:${DOMAIN}\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth";
+
+	#openssl req -x509 -sha256 -out $CRT -keyout $KEY -newkey $ENCRYPT -nodes -sha256 -subj $SUBJ -extensions EXT -config <(printf $CONFIG);
+	#openssl req -x509 -sha256 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365
+}
+
+
+
 
 echo 'commands have been sourced'
--- pretix/docker-entrypoint.sh
+++ pretix/docker-entrypoint.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Sleep 1 sec: Avoid racecondition with db"
-sleep 1
+echo "Sleep to avoid race condition with db"
+sleep 2
 
 python3 server.py
-# python3 manage.py compilemessages
-# python3 manage.py compilejsi18n
-# python3 manage.py collectstatic --noinput
-# python3 manage.py compress
-# python3 -m pretix migrate --noinput
-# uwsgi uwsgi.ini
