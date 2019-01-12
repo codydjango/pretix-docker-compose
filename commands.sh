75353308f5f844166fd8e5f8f5218378ff4010ca
Cody Django Redmond <codydjango@gmail.com>
2019-01-11 16:38:21 -0800

    formatted

--- commands.sh
+++ commands.sh
@@ -5,7 +5,6 @@ alias dcu="docker-compose up"
 alias dcd="docker-compose down"
 alias dcl="docker-compose logs -f"
 
-
 function generateCert {
 	# https://letsencrypt.org/docs/certificates-for-localhost/
 	# https://github.com/jwilder/nginx-proxy
@@ -14,15 +13,9 @@ function generateCert {
 	DOMAIN=$1;
 	KEY="${DOMAIN}.key";
 	CRT="${DOMAIN}.crt";
-	ENCRYPT="rsa:2048"; #"rsa:2048";"4096"
+	ENCRYPT="rsa:2048";
 	SUBJ="/CN=${DOMAIN}";
 	CONFIG="[dn]\nCN=${DOMAIN}\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:${DOMAIN}\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth";
 
-	#openssl req -x509 -sha256 -out $CRT -keyout $KEY -newkey $ENCRYPT -nodes -sha256 -subj $SUBJ -extensions EXT -config <(printf $CONFIG);
-	#openssl req -x509 -sha256 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365
+	openssl req -x509 -sha256 -out $CRT -keyout $KEY -newkey $ENCRYPT -nodes -sha256 -subj $SUBJ -extensions EXT -config <(printf $CONFIG);
 }
-
-
-
-
-echo 'commands have been sourced'
